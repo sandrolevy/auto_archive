19bd0fb42118238e6398d43e1ebbce0bc5c84ab0
Sandro Levy <sandrojunior.peng@poli.ufrj.br>
2019-10-17 23:38:09 -0300

    Create folders_tree.py and implement a observer

--- files-organizer.py
+++ files-organizer.py
@@ -1,54 +1,39 @@
-import os
-import mimetypes
+from watchdog.observers import Observer
+import time
+from watchdog.events import FileSystemEventHandler
+import os 
+import json
 import shutil
+from datetime import datetime
+from time import gmtime, strftime
+from Modules import folders_tree
 
-def create_category_folders():
-    os.chdir(folder_to_track)
-    folders_to_create = [
-        'Images', 'Video', 'Audio', 'Documents', 'Programs', 'Compressed',
-        ]
-    for folders in folders_to_create:
-        if not os.path.exists(folders):
-            os.makedirs(folders)
+class MyHandler(FileSystemEventHandler):
+    def move_file(self):
+        folder_to_track = folders_tree.folder_to_track()
+        extensions_folders = folders_tree.folders()
+        os.chdir(folder_to_track)
+        for filename in os.listdir(folder_to_track):
+            try:
+                extension = str(os.path.splitext(folder_to_track + '/' + filename)[1])
+                path = extensions_folders[extension]
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                shutil.move(filename, path + '/' + filename)
+            except Exception:
+                extension = 'noname'
+    def on_modified(self, event):
+        self.move_file()
 
-def scan_folders():
-    os.chdir(folder_to_track)
-    for root, dirs, files in os.walk("."):
-        for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print(os.path.join(root, name))
+extensions_folders = folders_tree.folders()
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folders_tree.folder_to_track(), recursive=True)
+observer.start()
 
-def create_dicionary_of_extensions():
-        mimetypes.init()
-        general_type = [
-            'image', 'video', 'audio', 'text',
-        ]
-        extensions = {}
-        for type_of_file in general_type:
-            extensions[type_of_file] = []            
-            for ext in mimetypes.types_map:
-                if mimetypes.types_map[ext].split('/')[0] == type_of_file:     
-                    extensions[type_of_file].append(ext)           
-        return extensions
-
-def move_file():
-    extensions = create_dicionary_of_extensions()
-    images = extensions['image']
-    video = extensions['video']
-    audio = extensions['audio']
-    for file in os.listdir(folder_to_track): 
-        if any(file.endswith(ext) for ext in images):
-            path = folder_to_track + '/' + 'Images'
-            shutil.move(file, path + '/' + file)
-        elif any(file.endswith(ext) for ext in video):
-            path = folder_to_track + '/' + 'Video'
-            shutil.move(file, path + '/' + file)
-        elif any(file.endswith(ext) for ext in audio):
-            path = folder_to_track + '/' + 'Audio'
-            shutil.move(file, path + '/' + file)
-
-
-folder_to_track = r'D:\Users\Levy\Downloads\Teste'
-create_category_folders()
-move_file()
+try:
+    while True:           
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
